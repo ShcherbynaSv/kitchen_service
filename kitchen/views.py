a07c157b8a1733a472490b4eb2b494491fa248d3
Svitlana Shcherbyna <sw.shcherbina@gmail.com>
2025-07-18 15:58:24 +0300

    Create templates folder with base.html, sidebar.html, index.html files in kitchen and includes folders. Change index view to work with templates

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,6 +1,14 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from kitchen.models import DishType, Ingredient, Cook, Dish
+
 
 def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("<h1>Kitchen Service</h1>")
+    context = {
+        "dish_types": DishType.objects.all().count(),
+        "ingredients": Ingredient.objects.all().count(),
+        "cooks": Cook.objects.all().count(),
+        "dishes": Dish.objects.all().count(),
+    }
+    return render(request, "kitchen/index.html", context=context)
