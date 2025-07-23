ac7ad5f0992da358d00e34bd1c23cd2932cf260d
Svitlana Shcherbyna <sw.shcherbina@gmail.com>
2025-07-23 10:51:30 +0300

    Create url, view and template to display dish type list page

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from kitchen.views import index
+from kitchen.views import index, DishTypeListView
 
 
 urlpatterns = [
     path("", index, name="index"),
+    path("dish-types/", DishTypeListView.as_view(), name="dish-type-list"),
 ]
 
 app_name = "kitchen"
